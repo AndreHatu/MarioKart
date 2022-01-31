9071550b0cd35c086a2ec72260fa051771caef8d
Andre Hatushikano <andrehatushikano@gmail.com>
2022-01-30 22:42:53 -0500

    added shared structures file

---
+++ config/mario_kart_config.c
@@ -0,0 +1,20 @@
+/* file to put shared data structures, functions and definitions*/
+
+#define CONFIG_ESPNOW_PMK "pmk1234567890"
+
+
+#define CONTROLLER_MAC_ADDR {0x3c, 0x61, 0x05, 0x7d, 0xe0, 0x88}
+#define CAR_MAC_ADDR {0x3c, 0x61, 0x05, 0x7d, 0xdd, 0xa4}
+
+
+typedef struct __attribute__((packed)) {
+	bool up;
+	bool down;
+	bool left;
+	bool right;
+} packet_t;
+
+typedef struct {
+	uint8_t sender_mac_addr[6];
+	packet_t data;
+} recv_packet_t;
