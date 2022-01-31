263dd560aa704b7547dfbdd610209586a93e98f1
Andre Hatushikano <andrehatushikano@gmail.com>
2022-01-30 23:11:44 -0500

    it works!

--- config/mario_kart_config.h
+++ config/mario_kart_config.h
@@ -1,7 +1,12 @@
 /* file to put shared data structures, functions and definitions*/
+#ifndef MARIO_KART_CONFIG
+#define MARIO_KART_CONFIG
+
+#include <inttypes.h>
+#include <stdbool.h>
 
 #define CONFIG_ESPNOW_PMK "pmk1234567890"
-
+#define MAC_LEN 6
 
 #define CONTROLLER_MAC_ADDR {0x3c, 0x61, 0x05, 0x7d, 0xe0, 0x88}
 #define CAR_MAC_ADDR {0x3c, 0x61, 0x05, 0x7d, 0xdd, 0xa4}
@@ -18,3 +23,5 @@ typedef struct {
 	uint8_t sender_mac_addr[6];
 	packet_t data;
 } recv_packet_t;
+
+#endif
