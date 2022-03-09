387dd46b68b6e6eef7f610b899fb0267d95cf4ba
Andre Hatushikano <andrehatushikano@gmail.com>
2022-03-09 13:32:47 -0500

    display init commit

---
+++ tower/main/display.h
@@ -0,0 +1,20 @@
+#ifndef DISPLAY_IF
+#define DISPLAY_IF
+
+#include "EVE.h"
+
+#define RED     0xff0000UL
+#define ORANGE  0xffa500UL
+#define GREEN   0x00ff00UL
+#define BLUE    0x0000ffUL
+#define BLUE_1  0x5dade2L
+#define YELLOW  0xffff00UL
+#define PINK    0xff00ffUL
+#define PURPLE  0x800080UL
+#define WHITE   0xffffffUL
+#define BLACK   0x000000UL
+
+
+void make_menu();
+
+#endif
